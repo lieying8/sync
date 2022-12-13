615c24264908934f36c37c6142d838d4d439cdbc
lieying8 <60979836+lieying8@users.noreply.github.com>
2022-12-13 19:56:17 +0800

    Update lw.js

--- lw.js
+++ lw.js
@@ -1,13 +1,9 @@
-/* 录屏 - 屏幕录制软件 https://apps.apple.com/cn/app/%E5%BD%95%E5%B1%8F-%E5%B1%8F%E5%B9%95%E5%BD%95%E5%88%B6%E8%BD%AF%E4%BB%B6/id1380506650
-
-附Qx规则
-#录丸 解锁会员 A+ 
+// 录丸 解锁会员 A+ 
+[rewrite_local]
 ^https:\/\/irecgo\.softin-tech\.com\/user\/info url script-response-body https://raw.githubusercontent.com/lieying8/sync/main/lw.js
 
-hostname
-irecgo.softin-tech.com
-
-*/
+[mitm]
+hostname = irecgo.softin-tech.com
 
 body = $response.body.replace(/expires_date_ms":\d+/g, 'expires_date_ms":1800000000111');
 $done({body});
