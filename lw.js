dde5d6c3f65ed9e9ba8f8c8b06f5b68f9f939a40
lieying8 <60979836+lieying8@users.noreply.github.com>
2022-12-13 20:06:58 +0800

    Update lw.js

--- lw.js
+++ lw.js
@@ -1,9 +1,22 @@
-// 录丸 解锁会员 A+ 
+/* 录丸 解锁会员 A+ 
 [rewrite_local]
 ^https:\/\/irecgo\.softin-tech\.com\/user\/info url script-response-body https://raw.githubusercontent.com/lieying8/sync/main/lw.js
 
 [mitm]
 hostname = irecgo.softin-tech.com
+*/
 
-body = $response.body.replace(/expires_date_ms":\d+/g, 'expires_date_ms":1800000000111');
+var body = $response.body;
+var url = $request.url;
+
+const path1 = '\/user\/info';
+
+let obj = JSON.parse(body);
+
+if (url.indexOf(path1) != -1) {
+	obj.data["expires_date_ms"] = 1800000000111;
+	body = JSON.stringify(obj);  
+ }
 $done({body});
+
+
