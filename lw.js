52fe88a2ffcfd04da78603f2f2a12de91d3b8c7a
lieying8 <60979836+lieying8@users.noreply.github.com>
2022-12-13 19:45:57 +0800

    Update lw.js

--- lw.js
+++ lw.js
@@ -2,7 +2,7 @@
 
 附Qx规则
 #录丸 解锁会员 A+ 
-^https:\/\/irecgo\.softin-tech\.com\/user\/info url script-response-body lw.js
+^https:\/\/irecgo\.softin-tech\.com\/user\/info url script-response-body https://raw.githubusercontent.com/lieying8/sync/main/lw.js
 
 hostname
 irecgo.softin-tech.com
