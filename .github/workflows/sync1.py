e29e54ec8a47a3d2ebd4afea43e07db9cab4678f
lieying8 <60979836+lieying8@users.noreply.github.com>
2023-06-16 22:42:02 +0800

    Update sync1.py

--- .github/workflows/sync1.py
+++ .github/workflows/sync1.py
@@ -1,1 +1,9 @@
+# your_script.py
+import requests
+
+response = requests.get('https://raw.githubusercontent.com/freefq/free/master/v2')
+html_content = response.text
+
+with open('.github/workflows/vv.txt', 'w') as f:
+    f.write(html_content)
 
