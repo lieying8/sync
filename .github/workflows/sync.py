25a2e8fc37d97a664649efd9ba8ad6f2df5e532d
lieying8 <60979836+lieying8@users.noreply.github.com>
2023-06-16 19:00:13 +0800

    Update sync.py

--- .github/workflows/sync.py
+++ .github/workflows/sync.py
@@ -1,1 +1,9 @@
+# your_script.py
+import requests
+
+response = requests.get('https://raw.fastgit.org/freefq/free/master/v2')
+html_content = response.text
+
+with open('index.html', 'w') as f:
+    f.write(html_content)
 
