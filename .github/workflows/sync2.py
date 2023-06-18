8ec726206659eb3a08732a8339c50dff0f75529a
lieying8 <60979836+lieying8@users.noreply.github.com>
2023-06-18 15:58:36 +0800

    Create sync2.py

---
+++ .github/workflows/sync2.py
@@ -0,0 +1,19 @@
+# sync2.py
+import requests
+
+# 请求A网站的内容
+response = requests.get('https://raw.fastgit.org/freefq/free/master/v2')
+html_content = response.text
+
+# 创建一个保存到B网站的链接，使用WebDAV
+webdav_url = 'http://file.966a.cn/vvv.txt'
+
+# 通过PUT请求将新内容保存到B网站
+response = requests.put(webdav_url, data=html_content)
+
+# 检查请求是否成功
+if response.status_code == 200:
+    print('File successfully updated.')
+else:
+    print('Error updating file:', response.status_code)
+2
