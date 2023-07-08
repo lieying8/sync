0af19b89dca5c408fb80661d4bf8992911f9b0b3
lieying8 <60979836+lieying8@users.noreply.github.com>
2023-07-08 16:04:46 +0800

    Update sync1.py

--- .github/workflows/sync1.py
+++ .github/workflows/sync1.py
@@ -1,10 +1,8 @@
 # https://github.com/freefq/free  
 # https://raw.fastgit.org/freefq/free/master/v2
 import requests
-# 禁用 SSL 证书验证
-requests.packages.urllib3.disable_warnings()
 
-response = requests.get('https://raw.fastgit.org/freefq/free/master/v2', verify=False)
+response = requests.get('https://raw.githubusercontent.com/freefq/free/master/v2')
 html_content = response.text
 
 with open('.github/workflows/vvv.txt', 'w') as f:
