b48ef15071ce5dcb10d59f3b0abb05818e68d61f
lieying8 <60979836+lieying8@users.noreply.github.com>
2024-06-11 00:25:40 +0800

    Update sync.py

--- .github/workflows/sync.py
+++ .github/workflows/sync.py
@@ -1,7 +1,7 @@
 # https://github.com/freefq/free
 import requests
 
-response = requests.get('https://raw.githubusercontent.com/freefq/free/master/v2')
+response = requests.get('https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub')
 html_content = response.text
 
 with open('vv.txt', 'w') as f:
