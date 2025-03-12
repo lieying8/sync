201b4d9c6c254c0d95690a3cb546185f590d5bd4
lieying8 <60979836+lieying8@users.noreply.github.com>
2025-03-12 23:02:55 +0800

    更新 xiaoyuan.js

--- xiaoyuan.js
+++ xiaoyuan.js
@@ -1,1 +1,10 @@
 
+/* 小猿AI 解锁会员
+
+[rewrite_local]
+^https:\/\/api\.yuanfudao\.com\/vip\/status url script-response-body https://raw.githubusercontent.com/lieying8/sync/main/xiaoyuan.js
+
+[mitm]
+hostname = api.yuanfudao.com
+*/
+
